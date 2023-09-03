6fb480a4358f1a0629627281652e30101312bac3
LAPTOP-O3L7EUHH\Juan Diego <avilameza@gmail.com>
2023-09-03 10:13:40 -0500

    carga inicial de los archivos del proyecto

---
+++ src/main/java/com/medvoll/appmedical/AppmedicalApplication.java
@@ -0,0 +1,13 @@
+package com.medvoll.appmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmedicalApplication.class, args);
+	}
+
+}
